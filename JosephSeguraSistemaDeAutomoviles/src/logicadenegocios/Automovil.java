79dafab5ef757f1ab0d6ade8ed8c98a3e9d5f0df
josephSD33 <136411783+josephSD33@users.noreply.github.com>
2024-02-26 23:19:40 -0600

    uijm

--- JosephSeguraSistemaDeAutomoviles/src/logicadenegocios/Automovil.java
+++ JosephSeguraSistemaDeAutomoviles/src/logicadenegocios/Automovil.java
@@ -20,49 +20,118 @@ public class Automovil {
         duenio = pDuenio;
         telefonoDuenio = pTelefonoDuenio;
         costo = generarCostoAleatorio();
-        placa = 0; // La placa se asignará automáticamente más adelante
+        placa = 0;
         fechaCompra = "Fecha de compra no especificada";
         isChocado = false;
     }
-    private int generarCostoAleatorio() {
-        Random rand = new Random();
-        return rand.nextInt((7000 - 3000) + 1) + 3000;
+     public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String pMarca) {
+        marca = pMarca;
+    }
+
+    public int getCosto() {
+        return costo;
+    }
+
+    public void setCosto(int pCosto) {
+        costo = pCosto;
+    }
+
+    public int getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(int pPlaca) {
+        placa = pPlaca;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String pColor) {
+        color = pColor;
+    }
+
+    public String getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(String pFechaCompra) {
+        fechaCompra = pFechaCompra;
+    }
+
+    public String getDuenio() {
+        return duenio;
+    }
+
+    public void setDuenio(String pDuenio) {
+        duenio = pDuenio;
+    }
+
+    public String getTelefonoDuenio() {
+        return telefonoDuenio;
+    }
+
+    public void setTelefonoDuenio(String pTelefonoDuenio) {
+        telefonoDuenio = pTelefonoDuenio;
+    }
+
+    public boolean isChocado() {
+        return isChocado;
+    }
+
+    public void setChocado(boolean pChocado) {
+        isChocado = pChocado;
     }
     
-    public void cambiarDuenio(String nuevoDuenio, String nuevoTelefono) {
+     public void cambiarDuenio(String pNuevoDuenio, String pNuevoTelefono) {
         if (!isChocado) {
-            duenio = nuevoDuenio;
-            telefonoDuenio = nuevoTelefono;
-            System.out.println("El cambio de duenno se ha realizado satisfactoriamente.");
+            duenio = pNuevoDuenio;
+            telefonoDuenio = pNuevoTelefono;
+            System.out.println("El cambio de dueño se ha realizado");
         } else {
-            System.out.println("El automovil está chocado. No se puede cambiar de dueño.");
+            System.out.println("El automóvil está chocado, no se puede cambiar ");
         }
     }
-    public void cambiarColor(String nuevoColor) {
-        if (!color.equals(nuevoColor)) {
-            color = nuevoColor;
-            System.out.println("El cambio de color se ha realizado satisfactoriamente.");
+
+
+     public void cambiarColor(String pNuevoColor) {
+        if (!pNuevoColor.equals(color)) {
+            color = pNuevoColor;
         } else {
-            System.out.println("El color nuevo es igual al anterior. No se permite el cambio.");
+            System.out.println("El color nuevo es igual al anterior, no se permite el cambio.");
         }
     }
-    public void chocarAutomovil() {
+     
+     public void chocarAutomovil() {
         isChocado = true;
     }
-    public void repararAutomovil() {
+     
+      public void repararAutomovil() {
         isChocado = false;
     }
-    public String toString() {
-        return "Automovil{" +
-                "marca='" + marca + '\'' +
-                ", costo=" + costo +
-                ", placa=" + placa +
-                ", color='" + color + '\'' +
-                ", fechaCompra='" + fechaCompra + '\'' +
-                ", duenio='" + duenio + '\'' +
-                ", telefonoDuenio='" + telefonoDuenio + '\'' +
-                ", isChocado=" + isChocado +
-                '}';
+      private int generarCostoAleatorio() {
+        Random rand = new Random();
+        return rand.nextInt(4001) + 3000;
     }
+      
     
+        
+      
+    
+     //falta toString
+      
+      
+      
+     
+     
+     
 }
+
+    
+
+
